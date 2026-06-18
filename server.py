f9eddb31cb778ae27b9abbf93dc95145fb86e083
mrsirquanzo <hoquan12@gmail.com>
2026-06-18 13:19:23 -0400

    Complete emotion detection project

--- server.py
+++ server.py
@@ -1,28 +1,41 @@
-''' Executing this function initiates the application of sentiment
-    analysis to be executed over the Flask channel and deployed on
-    localhost:5000.
-'''
-# Import Flask, render_template, request from the flask pramework package : TODO
-# Import the sentiment_analyzer function from the package created: TODO
+"""
+Flask server for the Emotion Detection application.
+Provides web routes to analyze text and return emotion scores.
+"""
 
-#Initiate the flask app : TODO
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
 
 @app.route("/sentimentAnalyzer")
-def sent_analyzer():
-    ''' This code receives the text from the HTML interface and 
-        runs sentiment analysis over it using sentiment_analysis()
-        function. The output returned shows the label and its confidence 
-        score for the provided text.
-    '''
-    # TODO
+def emo_detector():
+    """Analyze the given text and return the emotion scores and dominant emotion."""
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    dominant_emotion = response['dominant_emotion']
+
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+
+    return f"For the given statement, the system response is 'anger': {anger}, \
+'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. \
+The dominant emotion is {dominant_emotion}."
+
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
-    #TODO
+    """Render the main index page of the application."""
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    app.run(host="0.0.0.0", port=5000)
+    
